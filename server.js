464ecf1b808697ecdf9e85e692fb2e941be6d14c
dev_chow <56135351+devchow@users.noreply.github.com>
2023-08-26 23:15:13 +0300

    update
    update

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const jwt = require('jsonwebtoken');
+
+app.use(express.json());
 
 const posts = [
     {
@@ -9,15 +14,49 @@ const posts = [
     {
         username: 'Jane',
         title: 'Post 45'
+    },
+    {
+        username: 'James',
+        title: 'Post 6'
     }
 ]
 
+// main page
 app.get('/', (req, res) => {
     res.send('Node js - Json Web Token - Authentication');
 });
 
+// get all posts
 app.get('/posts', (req, res) => {
-    res.json(posts);
+    res.json(posts.filter(post => post.username === req.user.name));
 });
+
+// login user
+app.post('/login', (req, res) => {
+    // Authenticate User
+
+    // Create JWT
+    const username = req.body.username;
+    const user = { name: username };
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
+});
+
+// auth middleware
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    // if we have a token, we split it and get the second part
+    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        // console.log(err);
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
 
 app.listen(3000);
